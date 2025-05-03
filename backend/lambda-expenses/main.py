ab2144e6fcaf0b6871c0cc5c849ba67f94d12b59
JuanDiaz <juanben10xlr99@gmail.com>
2025-05-03 23:22:46 +0200

    chore: main for prediction as well

--- backend/lambda-expenses/main.py
+++ backend/lambda-expenses/main.py
@@ -1,12 +1,13 @@
 import json
 import os
 from expenses_classifier import classify_expenses
-from expenses_predictor import get_current_month_expenses
+from expenses_predictor import predict_expenses
 from utils import error_handler, response_creator, create_s3_client, get_csv_data
 
 AWS_ACCESS_KEY_ID     = ""
 AWS_SECRET_ACCESS_KEY = ""
-MODEL_BUCKET = 'expenses-classifier-model'
+CLASSIFIER_MODEL_BUCKET = 'expenses-classifier-model'
+PREDICTOR_MODEL_BUCKET = 'expenses-predictor-model'
 DATA_BUCKET  = 'datasets-expenses'
 CSV_DATA    = 'expenses.csv'
 
@@ -40,42 +41,15 @@ def handler(event, context):
     
     if action == 'classify-expenses':
         try:
-            classified_expenses = classify_expenses(s3_client, data, MODEL_BUCKET, DATA_BUCKET, CSV_DATA)
+            classified_expenses = classify_expenses(s3_client, data, CLASSIFIER_MODEL_BUCKET, DATA_BUCKET, CSV_DATA)
             return response_creator(200, classified_expenses)
         except Exception as e:
             error_handler(e, "Failed to classify expenses")
             return
     elif action == 'predict-expenses':
         try:
-            # predicted_expenses = predict_expenses(data)
-            #return response_creator(200, predicted_expenses)
-
-             # try:
-    #     # Get expenses data for current month
-    #     expenses_data = get_current_month_expenses()
-        
-    #     # Return the result
-    #     return {
-    #         'statusCode': 200,
-    #         'headers': {
-    #             'Content-Type': 'application/json'
-    #         },
-    #         'body': json.dumps({
-    #             'expensesPerDayCurrentMonth': expenses_data
-    #         })
-    #     }
-    # except Exception as e:
-    #     print(f"Error processing expense data: {str(e)}")
-    #     return {
-    #         'statusCode': 500,
-    #         'headers': {
-    #             'Content-Type': 'application/json'
-    #         },
-    #         'body': json.dumps({
-    #             'error': f'Failed to process expense data: {str(e)}'
-    #         })
-    #     }
-            return
+            predicted_expenses = predict_expenses(s3_client, data, PREDICTOR_MODEL_BUCKET)
+            return response_creator(200, predicted_expenses)
         except Exception as e:
             error_handler(e, "Failed to predict expenses")
             return  
